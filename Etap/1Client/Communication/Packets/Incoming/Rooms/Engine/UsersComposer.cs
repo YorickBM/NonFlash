5a643c3f7cd8f02b637060b6b3ea9f8e1c35a23b
Yorick Blom <yorickblom@hotmail.com>
2020-12-12 14:57:53 +0100

    Meubi offsets (No rotation or Interaction ye)

---
+++ Etap/1Client/Communication/Packets/Incoming/Rooms/Engine/UsersComposer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Etap.Hotel.GameClients;
+
+namespace Etap.Communication.Packets.Incoming.Rooms.Engine
+{
+    class UsersComposer : IPacketEvent
+    {
+        public void Parse(GameClient session, ClientPacket packet)
+        {
+            int users = packet.PopInt();
+            for(int i = 0; i < users; i++)
+            {
+
+            }
+        }
+    }
+}
